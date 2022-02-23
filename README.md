6e8541092db8aa596439251ca5fd5b040321a4b3
woaixiaoyuyu <519902366@qq.com>
2022-02-23 10:14:00 +0800

    new readme

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ A collection of some projects during learning c++.
 学完基础语法后，做的第一个小项目，模拟了一个vector，底层用数组实现。
 
 可以练习基础的class，友元，重载，模版类，模版特化等方法。
+
+## MyDB
+
+想做一个简单的data base，参考链接：https://cstack.github.io/db_tutorial/
+
+更多是了解一下数据库具体有哪些组件，相关概念还需要研读很多书籍才行。
--- myDB/myDB/repl.cpp
+++ myDB/myDB/repl.cpp
@@ -54,7 +54,16 @@ int main(int argc, const char * argv[]) {
             exit(EXIT_SUCCESS);
         } else {
             cout << "Unrecognized command : " << buff->buff << endl;
+            // Non-SQL statements like .exit are called “meta-commands”. They all start with a dot, so we check for them and handle them in a separate function.
+            // 判断指令是否存在，或者说指令是否符合标准
         }
     }
+    
+    // Next, we add a step that converts the line of input into our internal representation of a statement. This is our hacky version of the sqlite front-end.
+    // 将指令转换成字节码
+    
+    // Lastly, we pass the prepared statement to execute_statement. This function will eventually become our virtual machine.
+    // 执行指令对应的字节码
+    
     return 0;
 }
