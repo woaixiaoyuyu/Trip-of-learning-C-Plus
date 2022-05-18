14e3ca5e1e19a11113026555355ce999260621a1
woaixiaoyuyu <519902366@qq.com>
2022-05-18 14:20:39 +0800

    fix

--- myTestForObjectModel/test2.cpp
+++ myTestForObjectModel/test2.cpp
@@ -11,7 +11,7 @@ class A : public Y, public Z {};
 class x {};
 class y : virtual public x {};
 class z : virtual public x {};
-class a : virtual public y, public z {};
+class a : public y, public z {};
 
 
 int main(int argc, char const *argv[]) {
@@ -24,6 +24,18 @@ int main(int argc, char const *argv[]) {
 	cout << sizeof(x) << endl;	// 1
 	cout << sizeof(y) << endl;	// 8
 	cout << sizeof(z) << endl;	// 8
-	cout << sizeof(a) << endl;	// 16
+	cout << sizeof(a) << endl;	// 16	
+
+	a p;
+
+	cout << &p << endl;
+
+	/**
+	(gdb) p p
+	$1 = {<y> = {<x> = {<No data fields>}, _vptr$y = 0x100001048 <vtable for a+24>}, <z> = {_vptr$z = 0x100001060 <VTT for a>}, <No data fields>}
+	(gdb)
+	可以看到有两个vptr，y和z的，继承的x来自于y
+	*/
+
 	return 0;
 }
