60095458d49d4443e494aa5a79e6b5a66148ff38
woaixiaoyuyu <519902366@qq.com>
2022-05-27 11:39:08 +0800

    threadsafe stack bugs

--- myConcurrency/main.cpp
+++ myConcurrency/main.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
+#include <thread>
 #include "my_accumulate.hpp"
+#include "threadsafe_stack.hpp"
 
 void test_my_parallel_accumulate() {
     std::vector<int> v(48, 1);
     int result = my_parallel_accumulate(v.begin(), v.end(), 3);
     std::cout << "result is : " << result << std::endl;
+}
+
+void push_elem_to_stack(threadsafe_stack<int>& p, int elem) {
+    p.push(elem);
+    std::cout << "size is : " << p.size() << std::endl;
+}
+
+void pop_elem_in_stack(threadsafe_stack<int>& p) {
+    p.pop();
+    std::cout << "size is : " << p.size() << std::endl;
+}
+
+void test_threadsafe_stack() {
+    threadsafe_stack<int> p;
+    std::thread t(push_elem_to_stack, std::ref(p), 3);
+    std::thread w(pop_elem_in_stack, std::ref(p));
+    std::thread q(push_elem_to_stack, std::ref(p), 10);
+    t.join();
+    w.join();
+    q.join();
 }
 
 int main() {
@@ -13,6 +35,8 @@ int main() {
      * 若不考虑共享内存可能会带来的问题，多线程将会成为主流语言(包括C++)更青睐的并发途径。
      * 此外，C++标准并未对进程间通信提供任何原生支持，所以使用多进程的方式实现，这会依赖与平台相关的API。
      * 因此，本书只关注使用多线程的并发，并且在此之后所提到“并发”，均假设为多线程来实现。*/
-    test_my_parallel_accumulate();
+
+    // test_my_parallel_accumulate();
+    test_threadsafe_stack();
     return 0;
 }
---
+++ myConcurrency/threadsafe_stack.hpp
@@ -0,0 +1,65 @@
+//
+// Created by xiaoyuyu on 2022/5/27.
+//
+
+#include <mutex>
+#include <stack>
+#include <memory>
+
+#ifndef MYCONCURRENCY_THREADSAFE_STACK_HPP
+#define MYCONCURRENCY_THREADSAFE_STACK_HPP
+
+template <typename T>
+class threadsafe_stack {
+private:
+    std::mutex m;
+    std::stack<T> s;
+public:
+    threadsafe_stack() : s(std::stack<T>()) {}
+    threadsafe_stack(const threadsafe_stack<T>& p) {
+        std::lock_guard<std::mutex> lock(m);    // 上锁，之后会自动释放
+        s = p.s;
+    }
+    threadsafe_stack& operator=(const threadsafe_stack<T>& p) = delete;
+public:
+    bool empty() {
+        std::lock_guard<std::mutex> lock(m);
+        return s.empty();
+    }
+    void push(T elem) {
+        std::lock_guard<std::mutex> lock(m);
+        s.push(elem);
+    }
+
+    int size() {
+        std::lock_guard<std::mutex> lock(m);
+        return s.size();
+    }
+
+    void pop(T& value) {
+        // 把弹出的值赋值给value
+        std::lock_guard<std::mutex> lock(m);
+        if (!empty()) {
+            T temp = s.top();
+            value = temp;
+            s.pop();
+        } else {
+            throw std::logic_error("can't pop empty stack.\n");
+        }
+    }
+    std::shared_ptr<T> pop() {
+        // 对于大规模的元素而言，直接返回副本会有大量的空间开销，所以返回指针
+        // 并且智能指针能帮助管理内存
+        std::lock_guard<std::mutex> lock(m);
+        if (!empty()) {
+            std::shared_ptr<T> p = std::make_shared<T>(s.top());
+            s.pop();
+            return p;
+        } else {
+            throw std::logic_error("can't pop empty stack.\n");
+        }
+    }
+};
+
+
+#endif //MYCONCURRENCY_THREADSAFE_STACK_HPP
